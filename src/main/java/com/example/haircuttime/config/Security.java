1ce21e300a0356ed55df7c6803c6858deadfe1f0
kamil <kamilgozdz23@gmail.com>
2023-02-20 19:05:45 +0100

    feature/adding endpioints to security config

--- src/main/java/com/example/haircuttime/config/Security.java
+++ src/main/java/com/example/haircuttime/config/Security.java
@@ -2,50 +2,52 @@ package com.example.haircuttime.config;
 
 import com.example.haircuttime.service.user.UserServiceImpl;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Security {
 
     private final UserServiceImpl userServiceImpl;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    //TODO DODAC ODPOWIEDNIE ENDPOINTY I ROLE DO FILTERCHAIN'A
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().disable().authorizeHttpRequests()
-//                .requestMatchers("")
-//                .hasRole("ADMIN")
-//                .requestMatchers("")
-//                .permitAll()
-//                .requestMatchers("")
-//                .permitAll()
-                .anyRequest().permitAll();
-//               .authenticated()
-//                .and()
-//                .formLogin()
-//                .defaultSuccessUrl("http://localhost:3000")
-//                .failureUrl("/http://localhost:3000:")
-//                .and()
-//                .logout().permitAll()
-//                .and()
-//                .httpBasic();
+                .authorizeRequests((request) -> request
+                        .requestMatchers("/user/**", "/absence/**", "/barber/**", "/products/**",
+                                "/appointment/**", "/workday/**", "/workyear/**", "/workdefinition/**")
+                        .hasAuthority("ADMIN")
+                        .requestMatchers("/absence", "/absenceByBarber", "/update/absence/{id}").hasAuthority("PERSONNEL")
+                        .requestMatchers("/user/add", "/user/delete/{id}", "/user/update",
+                                "/products", "/barber/all", "/addAppointment",
+                                "/deleteAppointment", "/updateAppointment").hasAuthority("USER")
+                        .anyRequest()
+                )
+                .formLogin()
+                .defaultSuccessUrl("http://localhost:3000")
+                .failureUrl("/http://localhost:3000:")
+                .and()
+                .logout().permitAll()
+                .and()
+                .csrf().disable()
+                .httpBasic();
         return httpSecurity.build();
     }
 
-    //CZY POTRZEBNE???
-//    protected void configure(AuthenticationManager authenticationManager) throws Exception {
-//        authenticationManager.authenticate((Authentication) daoAuthenticationProvider());
-//    }
+    // CZY POTRZEBNE???
+    protected void configure(AuthenticationManager authenticationManager) throws Exception {
+        authenticationManager.authenticate((Authentication) daoAuthenticationProvider());
+    }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
